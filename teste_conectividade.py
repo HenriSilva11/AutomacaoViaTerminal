11069b8856a5f8677b8f324821979f37128ad0d5
carlos <pequenohenri72@gmail.com>
2025-10-29 12:49:43 -0300

    adicionando o envio por email

--- teste_conectividade.py
+++ teste_conectividade.py
@@ -1,18 +1,39 @@
+
 import os
-
-print("diretório atual: ", os.getcwd())
-
-arquivos = os.listdir()
-
-print("arquivos e pastas aqui: ", arquivos)
+import subprocess
+from email.mime.text import MIMEText
+import smtplib
 
 #executando e capturando retorno
 import subprocess
 
-resultado = subprocess.run(["cmd", "/c", "dir"], capture_output=True, text=True)
-print("saida do comando: ", resultado.stdout)
+ping = subprocess.run(["ping", "google.com"], capture_output=True, text=True)
+resultado_ping = ping.stdout
 
-#verificar status
+print("saida do comando: ", ping.stdout)
+
+#envio para email
+
+email_remetente = "pequenohenri72@gmail.com"
+senha_remetente = "nufr mcxw cptt towj"
+email_destino = "victorribeirodesouza10@gmail.com"
+
+msg = MIMEText(f"Resultado do ping:\n\n{resultado_ping}")
+msg["subject"] = "Resultado do ping"
+msg["from"] = email_remetente
+msg["to"] = email_destino
+
+try: 
+   server = smtplib.SMTP("smtp.gmail.com", 587)
+   server.starttls()
+   server.login(email_remetente, senha_remetente)
+   server.send_message(msg)
+   server.quit()
+except Exception as e:
+    print("Erro ao enviar o email: ", e)
+
+
+#verificar status e printar no terminal
 
 comando = subprocess.run(["ping", "-n", "1", "google.com"])
 
@@ -20,8 +41,4 @@ if comando.returncode == 0:
     print("conexão ativa!")
 else:
     print("sem conexão com a internet. ")
-
-
-
-
 
